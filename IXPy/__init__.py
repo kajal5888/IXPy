ec1e308452bc94ce9b491bcc355c67006686b6b4
kajal5888 <kajal5888@yahoo.co.in>
2025-09-12 16:59:07 +0200

    Calling internal definitions updated

--- IXPy/__init__.py
+++ IXPy/__init__.py
@@ -1,21 +1,14 @@
-# from IXPy.scripts.utils import utils
-# from IXPy.scripts.io import load_EEG
 
-# from .utils.utils import ReadCSV
-# from .io.load_EEG import load_EEG
 
 from .utils import *
 from .io import *
 from .preproc import *
 
-# # print(help(ReadCSV))
-# # Manage user-exposed namespace imports
-# # __all__ = ["ReadCSV", "load_EEG"]
+from . import utils
+from . import io
+from . import preproc
+
 __all__ = []
 __all__.extend(utils.__all__)
 __all__.extend(io.__all__)
 __all__.extend(preproc.__all__)
-# from IXPy.io.load_EEG import Load_Data
-# from IXPy.utils.utils import DJ_Print
-
-# __all__ = ["Load_Data", "DJ_Print"]

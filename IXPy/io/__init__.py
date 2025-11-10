6a5016848ca3ce1f10156ec4207a8222e102161c
kajal5888 <kajal5888@yahoo.co.in>
2025-11-10 15:50:10 +0100

    checking and including both def and class

--- IXPy/io/__init__.py
+++ IXPy/io/__init__.py
@@ -1,10 +1,10 @@
 import inspect
-from . import load_EEG, load_Psychopy
+from . import load_EEG, load_Psychopy, load_EmbracePlus
 
 __all__ = []
-for module in [load_EEG, load_Psychopy]:
+for module in [load_EEG, load_Psychopy, load_EmbracePlus]:
     funcs = [name for name, obj in inspect.getmembers(
-        module, inspect.isfunction) if obj.__module__ == module.__name__]
+        module, lambda o: inspect.isfunction(o) or inspect.isclass(o)) if getattr(obj, "__module__", None) == module.__name__]
     __all__.extend(funcs)
     for name in funcs:
         globals()[name] = getattr(module, name)

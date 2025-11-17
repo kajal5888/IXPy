7fcbdb099dfe09944418db53352f202227e25e7e
kajal5888 <kajal5888@yahoo.co.in>
2025-11-17 15:42:37 +0100

    Spectrogram MNE and sony Dab method included

--- IXPy/preproc/preprocessing.py
+++ IXPy/preproc/preprocessing.py
@@ -126,14 +126,114 @@ def PSD(raw, subject_id, channel=None, fmin=1, fmax=100, tmin=None, tmax=None,
                 plt.show(block=False)
 
 
-def Spectrogram(data, fmin, fmax, fs, nperseg, noverlap, normalize=True):
-    from scipy.signal import spectrogram
-    import numpy as np
-    frequencies, times_stft, Zxx = spectrogram(
-        data, fs=fs, nperseg=nperseg, noverlap=noverlap)
-    freq_mask = (frequencies >= fmin) & (frequencies <= fmax)
-    frequencies = frequencies[freq_mask]
-    power = Zxx[freq_mask, :]
-    if normalize:
-        power = (power - np.min(power)) / (np.max(power) - np.min(power))
-    return power, frequencies, times_stft
+def Spectrogram(raw, subject_id, channel_names, fmin, fmax, tmin, tmax, fs, nperseg, noverlap, save_full_path, save, normalize, overwrite, tfr_method, version_type, show):
+    """
+    Compute and save spectrograms for specified EEG channels using Short-Time Fourier Transform (STFT).
+
+    Args:
+        raw (mne.io.Raw): The MNE Raw object containing EEG data.
+        channel_names (list of str): List of channel names for which spectrograms will be generated.
+        fmin (float): Minimum frequency (Hz) for the spectrogram.
+        fmax (float): Maximum frequency (Hz) for the spectrogram.
+        fs (float): Sampling frequency (Hz) of the EEG data.
+        nperseg (int): Number of samples per segment for STFT.
+        noverlap (int): Number of overlapping samples between segments.
+        save_full_path (str): Directory path where the spectrogram images will be saved.
+        save (bool, optional): Whether to save the spectrogram plots. Defaults to True.
+        normalize (bool, optional): Whether to normalize the power values before visualization. Defaults to True.
+
+    Returns:
+        None
+
+    Notes:
+        - Uses `compute_spectrogram()` to calculate power spectral density.
+        - Normalizes power values using `normalize_data()` if `normalize=True`.
+        - Saves the spectrogram plots in the `save_full_path + '/Spectrogram/'` directory if `save=True`.
+        - Uses a logarithmic color scale (`10 * np.log10(power)`) for better visualization of power levels.
+    """
+    if utils.DirCheck(save_full_path, overwrite):
+        import numpy as np
+        import matplotlib.pyplot as plt
+        for ch in channel_names[-1:]:
+            utils.DJ_Print(
+                f"Generating Spectrogram for Subject: {subject_id}, channel: {ch}")
+            TFR_Filename = f"{save_full_path}{version_type}_{tfr_method}_{ch}.png"
+            fmin, fmax, fs = fmin, fmax, raw.info['sfreq']
+            frequencies = np.arange(fmin, fmax, .1)
+            n_cycles = 8
+
+            if version_type == 'SonyDAB':
+                from scipy.signal import spectrogram
+                data, times = raw.copy().get_data(
+                    picks=[ch], return_times=True)
+                # Get the 1D signal of the chosen channel
+                data = data[0]
+
+                # Perform STFT to get spectrogram
+                frequencies, times_stft, Zxx = spectrogram(
+                    data, fs=fs, nperseg=nperseg, noverlap=noverlap)
+                freq_mask = (frequencies >= fmin) & (frequencies <= fmax)
+                frequencies = frequencies[freq_mask]
+                power = Zxx[freq_mask, :]
+                if normalize:
+                    power = (power - np.min(power)) / \
+                        (np.max(power) - np.min(power))
+
+                vmin, vmax = -60, -10  # -140, -20
+
+                plt.figure(figsize=(10, 6))
+                plt.pcolormesh(times_stft, frequencies, 10 * np.log10(power),
+                               shading='gouraud', cmap='jet', vmin=vmin, vmax=vmax)
+                plt.colorbar(label='Power (dB)')
+                plt.title(
+                    f' {subject_id}: Time-Frequency Representation (STFT) for {ch}')
+                plt.xlabel('Time (s)')
+                plt.tight_layout()
+                # plt.show()
+
+                if save:
+                    plt.savefig(TFR_Filename, format='.png')
+            elif version_type == 'MNE_Python':
+
+                # Create a fake event spanning the entire raw
+                event_id = 1
+                events = np.array([[0, 0, event_id]])
+                tmin, tmax = 0, raw.times[-1]
+
+                # One long "epoch" only with target channels
+                Data_Epoched = mne.Epochs(raw.copy(), events, event_id=event_id, tmin=tmin, tmax=tmax,
+                                          baseline=None, preload=True, picks=ch)
+
+                tfr = utils.TFR_MNE(
+                    Filename=TFR_Filename.replace(".png", ""))
+                if tfr_method == 'Morlet':
+                    if os.path.isfile(tfr.Filename):
+                        DataTFR = tfr.Load()
+                    else:
+                        # DataTFR = mne.time_frequency.tfr_morlet(Data_Epoched, freqs=frequencies, n_cycles=n_cycles,
+                        #                                         use_fft=True, return_itc=False, decim=5, n_jobs=1)
+                        DataTFR = Data_Epoched.compute_tfr(method="morlet", freqs=frequencies, n_cycles=n_cycles,
+                                                           use_fft=True, return_itc=False, decim=5, n_jobs=1)
+                        tfr.Save(DataTFR)
+                elif tfr_method == 'MultiTaper':
+                    if os.path.isfile(f"{tfr.Filename}-tfr.h5"):
+                        DataTFR = tfr.Load()
+                    else:
+                        DataTFR = mne.time_frequency.tfr_multitaper(Data_Epoched, freqs=frequencies, time_bandwidth=4, n_cycles=n_cycles,
+                                                                    return_itc=False, decim=5, n_jobs=1)
+                        tfr.Save(DataTFR)
+                else:
+                    utils.DJ_Print(
+                        "Requested TFR method is not implemented", "warning")
+
+                vmin, vmax,  Yscale, cb, CMAP = (
+                    DataTFR.data.min()*.5, DataTFR.data.max()*.2,  'linear', False, plt.cm.jet)
+                plt.close('all')
+                plt.figure(figsize=(10, 8))
+                Ax = plt.axes()
+                DataTFR.plot(picks=ch, colorbar=cb, yscale=Yscale,
+                             cmap=CMAP, axes=Ax, show=False)
+                if save:
+                    plt.savefig(TFR_Filename, format='png')
+                if show:
+                    plt.show(block=False)
